18a1e90c4c59f55e102afc67e6b6909b7c1d2717
Yukito Ito <yukibukiyou@gmail.com>
2018-07-12 13:45:18 +0900

    fix

--- hook.rb
+++ hook.rb
@@ -1,8 +1,6 @@
 
 require "sinatra"
 
-sha1 = "sha1=41089c213457a147289e4b06f5cbb5cd7cc13680"
-
 post '/' do
   if request.env["HTTP_X_GITHUB_EVENT"] != "push"
     halt 200, "Event is not push"
@@ -10,11 +8,6 @@ post '/' do
 
   # TODO: 署名を検証
   puts request.env["HTTP_X_HUB_SIGNATURE"]
-
-  # FIXME
-  if request.env["HTTP_X_HUB_SIGNATURE"] != sha1
-    halt 401
-  end
 
   request.body.rewind
   data = JSON.parse request.body.read
