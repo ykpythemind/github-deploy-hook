2024b5bb0da49ba63d1d40f7fb6a906c95b74db9
Yukito Ito <yukibukiyou@gmail.com>
2018-07-13 02:25:27 +0900

    update

--- hook.rb
+++ hook.rb
@@ -3,13 +3,20 @@ require "sinatra"
 
 SCRIPT_FILE = (ENV["SCRIPT_FILE"] || "test.sh").freeze
 SCRIPT_DIR = 'scripts'.freeze
-SCRIPT_PATH = File.expand_path("../#{SCRIPT_DIR}/#{SCRIPT_FILE}", __FILE__)
+
+if ENV["SCRIPT_PATH"]
+  SCRIPT_PATH = ENV["SCRIPT_PATH"]
+else
+  SCRIPT_PATH = File.expand_path("../#{SCRIPT_DIR}/#{SCRIPT_FILE}", __FILE__)
+end
 
 abort "SCRIPT_FILE not found" unless File.exist? SCRIPT_PATH
 
+LOG_PATH = ENV["LOG_PATH"]
+
 post '/' do
   if request.env["HTTP_X_GITHUB_EVENT"] != "push"
-    halt 200, "Event is not push"
+    halt 304, "Event is not push"
   end
 
   # TODO: 署名を検証
@@ -19,11 +26,11 @@ post '/' do
   payload = JSON.parse request.body.read
 
   if payload["ref"] != "refs/heads/master"
-    halt 200, "Not master branch"
+    halt 304, "Not master branch"
   end
 
   if skip_script?(payload)
-    halt 200, "Skipped."
+    halt 304, "Skipped."
   end
 
   spawn "#{SCRIPT_PATH}"
@@ -34,6 +41,17 @@ end
 get '/' do
   body "alive"
 end
+
+get '/log' do
+  unless File.exist? LOG_PATH
+    halt 404, "Not found log file"
+  end
+
+  # tail したいので手抜き
+  lines = `tail -n 100 #{LOG_PATH}`
+  body lines
+end
+
 
 def skip_script?(payload)
   commits = payload["commits"]
