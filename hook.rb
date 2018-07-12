f5eb39a64e0ead62be75d3b789075e5d3b14e12d
Yukito Ito <yukibukiyou@gmail.com>
2018-07-12 16:13:32 +0900

    deploy scripts

--- hook.rb
+++ hook.rb
@@ -1,6 +1,12 @@
 
 require "sinatra"
 
+DEPLOY_SCRIPT_FILE = ENV["DEPLOY_SCRIPT_FILE"] || "test.sh"
+DEPLOY_SCRIPT_DIR = 'scripts'
+DEPLOY_SCRIPT_PATH = File.expand_path("../#{DEPLOY_SCRIPT_DIR}/#{DEPLOY_SCRIPT_FILE}", __FILE__)
+
+abort "DEPLOY_SCRIPT_FILE not found" unless File.exist? DEPLOY_SCRIPT_PATH
+
 post '/' do
   if request.env["HTTP_X_GITHUB_EVENT"] != "push"
     halt 200, "Event is not push"
@@ -16,6 +22,10 @@ post '/' do
     halt 200, "Not master branch"
   end
 
-  body "Hello World"
+  result = `#{DEPLOY_SCRIPT_PATH}`
+  result += "\nexitstatus: #{$?.exitstatus}"
+
+  puts result
+  body result
 end
 
